b10f8e9423c76a863de0d60783991b978b411d8f
meso272 <liujinyang1201@126.com>
2020-05-08 04:37:03 -0700

    dd

--- sequence.h
+++ sequence.h
@@ -637,7 +637,7 @@ namespace sequence {
     intT l = nblocks(n, b);
     b = nblocks(n, l);
     */
-    intT l=16;
+    intT l=32;
     intT b=nblocks(n,l);
     intT *Sums = newA(intT,l + 1);
     {parallel_for (intT i = 0; i < l; i++) {
