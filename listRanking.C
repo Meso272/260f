06e98bb05de02cee97a9f282a38dc0d8d18bf1e7
meso272 <liujinyang1201@126.com>
2020-05-25 17:20:12 -0700

    dd

--- listRanking.C
+++ listRanking.C
@@ -27,6 +27,8 @@
 #include "speculative_for2.h"
 #include "listRanking.h"
 #include "randPerm.C"
+#include "get_time.h"
+#include "parseCommandLine.h"
 using namespace std;
 
 struct listRankingStep {
@@ -57,6 +59,84 @@ void listRanking(node *A, intT n, intT r = -1) {
   free(R);
 }
 
-int main(){
-    return 0;
+
+
+
+void timeListRanking(node* A, intT n, int rounds, intT ratio) {
+  node* B = newA(node,n);
+  parallel_for(intT i=0;i<n;i++) {
+    B[i].prev = A[i].prev;
+    B[i].next = A[i].next;
+  }
+  listRanking(B, n, ratio);
+  double time=0;
+  for (int i=0; i < rounds; i++) {
+    parallel_for(intT i=0;i<n;i++) {
+      B[i].prev = A[i].prev;
+      B[i].next = A[i].next;
+    }
+    timer t; t.start();
+    listRanking(B, n, ratio);
+    t.stop();
+    time+=t.get_total()/double(rounds);
+  }
+  std::cout << time<<endl;
+  free(B);
+}
+
+struct addG { 
+  intT* A;
+  addG(intT* _A): A(_A) {}
+  intT operator() (const intT i) { return A[i] == i; }
+};
+
+int main(int argc, char* argv[]) {
+  commandLine P(argc,argv,"[-r <rounds>] <n>");
+  int rounds = P.getOptionIntValue("-r",1);
+  intT ratio = P.getOptionIntValue("-ratio",-1);
+  intT n = atoi(P.getArgument(0));
+  //intT numLists = min(n,P.getOptionIntValue("-l",1));
+  intT* A = newA(intT,n);
+  parallel_for(intT i=0;i<n;i++) A[i] = i;
+  randPerm(A, n, -1);
+
+  bool* processed = newArray(n,(bool)0);
+
+  //get rid of cycles
+  for(intT i=0;i<n;i++) {
+    if(!processed[i]) {
+      intT j = i;
+      while(A[j] != j) {
+    processed[j] = 1;
+    j = A[j];
+    if(j == i) { A[i] = i; break; }
+      }
+    }
+  }
+  free(processed);
+  intT nl = sequence::reduce<intT>((intT)0, n, utils::addF<intT>(), addG(A));
+  cout<<"num lists = "<<nl<<endl;
+  // intT i = 0;
+  // while(nl < numLists) {
+  //   if(A[i] != i) {
+  //     A[i] = i; nl++;
+  //   }
+  //   i++;
+  // }
+
+  //for(int i=0;i<n;i++)cout<<A[i]<<" ";cout<<endl;
+  node* nodes = newA(node,n);
+  parallel_for(intT i=0;i<n;i++) nodes[i].next = nodes[i].prev = n;
+  parallel_for(intT i=0;i<n;i++) {
+    if(A[i] != i) { 
+      nodes[i].next = A[i];
+      nodes[A[i]].prev = i;
+    }
+  }
+  
+  //last node of list has next = n, first node of list has prev = n
+
+  free(A);
+  timeListRanking(nodes, n, rounds, ratio);
+  free(nodes);
 }
